eb6e15e9b35ee39fce4ef88e79b80e092de0d32b
M. Ramandha Kurniawan Batubara <103089301+Pressureds@users.noreply.github.com>
2023-03-20 23:27:07 +0800

    Add files via upload

---
+++ Prakktikum 1/PRAK104.php
@@ -0,0 +1,33 @@
+<html>
+    <head>
+        <style>
+            table, td{
+                border : 1px solid;  
+            }
+            th{
+            font: bold;
+            border: 1px solid;
+            }
+        </style>
+    </head>
+    <body>
+        <table>
+                <tr><th>Daftar Smartphone Samsung</th></tr>
+            <?php 
+                $samsung = array("Samsung Galaxy S22", "Samsung Galaxy S22+", "Samsung Galaxy A03", "Samsung Galaxy Xcover 5")
+            ?>
+            <tr>
+                <td><?php echo $samsung[0]?></td>
+            </tr>
+            <tr>
+                <td><?php echo $samsung[1]?></td>
+            </tr>
+            <tr>
+                <td><?php echo $samsung[2]?></td>
+            </tr>
+            <tr>
+                <td><?php echo $samsung[3]?></td>
+            </tr>
+       </table>
+    </body>
+</html>
