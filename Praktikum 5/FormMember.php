644f94a9537f8244043f360f888e2c399d351359
M. Ramandha Kurniawan Batubara <103089301+Pressureds@users.noreply.github.com>
2023-05-16 07:46:54 +0800

    Update FormMember.php

--- Praktikum 5/FormMember.php
+++ Praktikum 5/FormMember.php
@@ -45,45 +45,7 @@ if (isset($_POST['kembali'])) {
 </head>
 
 <body>
-	<?php if (isset($_GET['id_member'])) : ?>
-		<h1 style="text-align: center;" class="mt-2">Form Edit Member</h1>
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-5 mx-auto">
-					<form method="POST">
-						<div class="mb-3 mt-3">
-							<label class="form-label">ID</label>
-							<input type="text" name="id_member" value="<?php echo $data['id_member']; ?>" placeholder="Isi ID Member" required class="form-control col-form-label-sm">
-						</div>
-						<div class="mb-3">
-							<label class="form-label">Nama Lengkap</label>
-							<input type="text" name="nama_member" value="<?php echo $data['nama_member']; ?>" placeholder="Isi Nama Lengkap Member" required class="form-control col-form-label-sm">
-						</div>
-						<div class="mb-3">
-							<label class="form-label">Nomor Telepon</label>
-							<input type="text" name="nomor_member" value="<?php echo $data['nomor_member']; ?>" placeholder="Isi Nomor Telepon" required class="form-control col-form-label-sm">
-						</div>
-						<div class="mb-3">
-							<label class="form-label">Alamat</label>
-							<input type="text" name="alamat" value="<?php echo $data['alamat']; ?>" placeholder="Isi Alamat" required class="form-control col-form-label-sm">
-						</div>
-						<div class="mb-3">
-							<label class="form-label">Tanggal Mendaftar</label>
-							<input type="datetime-local" step="1" name="tgl_mendaftar" value="<?php echo date('Y-m-d\TH:i:s', strtotime($data['tgl_mendaftar'])); ?>" placeholder="Isi Tanggal Pendaftaran" required class="form-control col-form-label-sm">
-						</div>
-						<div class="mb-3">
-							<label class="form-label">Tanggal Terakhir Bayar</label>
-							<input type="date" name="tgl_terakhir_bayar" value="<?php echo $data['tgl_terakhir_bayar']; ?>" placeholder="Isi Tanggal Terakhir Bayar" required class="form-control col-form-label-sm">
-						</div>
-						<button type="submit" class="btn btn-success" name="edit" onclick="return confirm('Ingin Menyimpan perubahan?')">Submit</button>
-						<button type="submit" value="ignore" formnovalidate class="btn btn-secondary" name="kembali">Kembali</button>
-					</form>
-				</div>
-			</div>
-		</div>
-	<?php
-	endif;
-	if (!isset($_GET['id_member'])) : ?>
+	<?php if (!isset($_GET['id_member'])) : ?>
 		<h1 style="text-align: center;" class="mt-2">Form Tambah Member</h1>
 		<div class="container">
 			<div class="row">
@@ -114,6 +76,44 @@ if (isset($_POST['kembali'])) {
 							<input type="date" name="tgl_terakhir_bayar" placeholder="Isi Tanggal Terakhir Bayar" required class="form-control col-form-label-sm">
 						</div>
 						<button type="submit" class="btn btn-success" name="submit" onclick="return confirm('Ingin menambah member?')">Submit</button>
+						<button type="submit" value="ignore" formnovalidate class="btn btn-secondary" name="kembali">Kembali</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	<?php
+	endif;
+	if (isset($_GET['id_member'])) : ?>
+		<h1 style="text-align: center;" class="mt-2">Form Edit Member</h1>
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-5 mx-auto">
+					<form method="POST">
+						<div class="mb-3 mt-3">
+							<label class="form-label">ID</label>
+							<input type="text" name="id_member" value="<?php echo $data['id_member']; ?>" placeholder="Isi ID Member" required class="form-control col-form-label-sm">
+						</div>
+						<div class="mb-3">
+							<label class="form-label">Nama Lengkap</label>
+							<input type="text" name="nama_member" value="<?php echo $data['nama_member']; ?>" placeholder="Isi Nama Lengkap Member" required class="form-control col-form-label-sm">
+						</div>
+						<div class="mb-3">
+							<label class="form-label">Nomor Telepon</label>
+							<input type="text" name="nomor_member" value="<?php echo $data['nomor_member']; ?>" placeholder="Isi Nomor Telepon" required class="form-control col-form-label-sm">
+						</div>
+						<div class="mb-3">
+							<label class="form-label">Alamat</label>
+							<input type="text" name="alamat" value="<?php echo $data['alamat']; ?>" placeholder="Isi Alamat" required class="form-control col-form-label-sm">
+						</div>
+						<div class="mb-3">
+							<label class="form-label">Tanggal Mendaftar</label>
+							<input type="datetime-local" step="1" name="tgl_mendaftar" value="<?php echo date('Y-m-d\TH:i:s', strtotime($data['tgl_mendaftar'])); ?>" placeholder="Isi Tanggal Pendaftaran" required class="form-control col-form-label-sm">
+						</div>
+						<div class="mb-3">
+							<label class="form-label">Tanggal Terakhir Bayar</label>
+							<input type="date" name="tgl_terakhir_bayar" value="<?php echo $data['tgl_terakhir_bayar']; ?>" placeholder="Isi Tanggal Terakhir Bayar" required class="form-control col-form-label-sm">
+						</div>
+						<button type="submit" class="btn btn-success" name="edit" onclick="return confirm('Ingin Menyimpan perubahan?')">Submit</button>
 						<button type="submit" value="ignore" formnovalidate class="btn btn-secondary" name="kembali">Kembali</button>
 					</form>
 				</div>
